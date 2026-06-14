1a3c0cb4c6211e9a769070c1f445470ebb70c254
YASSERRMD <arafath.yasser@gmail.com>
2026-06-14 23:48:47 +0400

    Add committee review API

---
+++ apps/backend/src/modules/committees/committees.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CommitteesService } from './committees.service';
+import { CommitteesController } from './committees.controller';
+
+@Module({
+  controllers: [CommitteesController],
+  providers: [CommitteesService],
+  exports: [CommitteesService],
+})
+export class CommitteesModule {}
